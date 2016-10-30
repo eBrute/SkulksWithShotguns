2435c5e7c25c72ef1a322af9d5cf2740cb0d723f
Brute <email.brute@gmail.com>
2016-10-30 19:16:15 +0100

    update to current version and fix missing function error

--- output/lua/ScoringMixin.lua
+++ output/lua/ScoringMixin.lua
@@ -1,15 +1,13 @@
-// added killstreak property.
+--[[
+======= Copyright (c) 2003-2011, Unknown Worlds Entertainment, Inc. All rights reserved. =======
 
+ lua\ScoringMixin.lua
+   ScoringMixin keeps track of a score. It provides function to allow changing the score.
+    Created by:   Brian Cronin (brianc@unknownworlds.com)
 
+========= For more information, visit us at http://www.unknownworlds.com =====================
+--]]
 
-
-
-
-
-
-/**
- * ScoringMixin keeps track of a score. It provides function to allow changing the score.
- */
 ScoringMixin = CreateMixin(ScoringMixin)
 ScoringMixin.type = "Scoring"
 
@@ -21,18 +19,33 @@ end
 
 ScoringMixin.networkVars =
 {
-    playerLevel = "private integer",
-    playerSkill = "private integer",
+    playerSkill = "integer",
+    playTime = "private time",
+    playerLevel = "integer",
+    totalXP = "integer",
+    teamAtEntrance = string.format("integer (-1 to %d)", kSpectatorIndex)
 }
 
 function ScoringMixin:__initmixin()
 
     self.score = 0
-    // Some types of points are added continuously. These are tracked here.
+    -- Some types of points are added continuously. These are tracked here.
     self.continuousScores = { }
-    
+
     self.serverJoinTime = Shared.GetTime()
-    
+
+    self.playerLevel = -1
+    self.totalXP = -1
+    self.playerSkill = -1
+
+    self.weightedEntranceTimes = {}
+    self.weightedEntranceTimes[kTeam1Index] = {}
+    self.weightedEntranceTimes[kTeam2Index] = {}
+
+    self.weightedExitTimes = {}
+    self.weightedExitTimes[kTeam1Index] = {}
+    self.weightedExitTimes[kTeam2Index] = {}
+
 end
 
 function ScoringMixin:GetScore()
@@ -41,26 +54,26 @@ end
 
 function ScoringMixin:AddScore(points, res, wasKill)
 
-    // Should only be called on the Server.
+    -- Should only be called on the Server.
     if Server then
-    
-        // Tell client to display cool effect.
-        if points ~= nil and points ~= 0 then
-        
+
+        -- Tell client to display cool effect.
+        if points and points ~= 0 and not GetGameInfoEntity():GetWarmUpActive() then
+
             local displayRes = ConditionalValue(type(res) == "number", res, 0)
             Server.SendNetworkMessage(Server.GetOwner(self), "ScoreUpdate", { points = points, res = displayRes, wasKill = wasKill == true }, true)
             self.score = Clamp(self.score + points, 0, self:GetMixinConstants().kMaxScore or 100)
-            
+
             if not self.scoreGainedCurrentLife then
                 self.scoreGainedCurrentLife = 0
             end
 
-            self.scoreGainedCurrentLife = self.scoreGainedCurrentLife + points    
+            self.scoreGainedCurrentLife = self.scoreGainedCurrentLife + points
 
         end
-    
+
     end
-    
+
 end
 
 function ScoringMixin:GetScoreGainedCurrentLife()
@@ -70,7 +83,11 @@ end
 function ScoringMixin:GetPlayerLevel()
     return self.playerLevel
 end
-  
+
+function ScoringMixin:GetTotalXP()
+    return self.totalXP
+end
+
 function ScoringMixin:GetPlayerSkill()
     return self.playerSkill
 end
@@ -78,8 +95,8 @@ end
 if Server then
 
     function ScoringMixin:CopyPlayerDataFrom(player)
-    
-        self.scoreGainedCurrentLife = player.scoreGainedCurrentLife    
+
+        self.scoreGainedCurrentLife = player.scoreGainedCurrentLife
         self.score = player.score or 0
         self.kills = player.kills or 0
         self.killstreak = player.killstreak or  0
@@ -89,10 +106,12 @@ if Server then
         self.commanderTime = player.commanderTime or 0
         self.marineTime = player.marineTime or 0
         self.alienTime = player.alienTime or 0
-        self.entranceTime = player.entranceTime
-        self.exitTime = player.exitTime
-		
-		self.teamAtEntrance = player.teamAtEntrance
+
+        self.weightedEntranceTimes = player.weightedEntranceTimes
+        self.weightedExitTimes = player.weightedExitTimes
+
+        self.teamAtEntrance = player.teamAtEntrance
+
         self.totalKills = player.totalKills
         self.totalAssists = player.totalAssists
         self.totalDeaths = player.totalDeaths
@@ -100,90 +119,97 @@ if Server then
         self.totalScore = player.totalScore
         self.totalPlayTime = player.totalPlayTime
         self.playerLevel = player.playerLevel
-        
+        self.totalXP = player.totalXP
+
     end
 
-    function ScoringMixin:OnKill()    
+    function ScoringMixin:OnKill()
         self.scoreGainedCurrentLife = 0
     end
-    
-    function ScoringMixin:GetPlayTime()
-        return self.playTime
-    end
-    
+
     function ScoringMixin:GetMarinePlayTime()
         return self.marineTime
     end
-    
+
     function ScoringMixin:GetAlienPlayTime()
         return self.alienTime
     end
-    
+
     function ScoringMixin:GetCommanderTime()
-        return self.commanderTime
+        return self.commanderTime or 0
     end
-    
+
     local function SharedUpdate(self, deltaTime)
-    
+
         if not self.commanderTime then
             self.commanderTime = 0
         end
-        
+
         if not self.playTime then
             self.playTime = 0
         end
-        
+
         if not self.marineTime then
             self.marineTime = 0
         end
-        
+
         if not self.alienTime then
             self.alienTime = 0
-        end    
-        
+        end
+
         if self:GetIsPlaying() then
-        
+
             if self:isa("Commander") then
                 self.commanderTime = self.commanderTime + deltaTime
             end
-            
+
             self.playTime = self.playTime + deltaTime
-            
+
             if self:GetTeamType() == kMarineTeamType then
                 self.marineTime = self.marineTime + deltaTime
             end
-            
+
             if self:GetTeamType() == kAlienTeamType then
                 self.alienTime = self.alienTime + deltaTime
             end
-        
+
         end
-    
+
     end
-    
+
     function ScoringMixin:OnProcessMove(input)
         SharedUpdate(self, input.time)
     end
-    
+
     function ScoringMixin:OnUpdate(deltaTime)
+        PROFILE("ScoringMixin:OnUpdate")
         SharedUpdate(self, deltaTime)
     end
 
 end
 
+function ScoringMixin:GetPlayTime()
+    return self.playTime
+end
+
+function ScoringMixin:GetLastTeam()
+    return self.teamAtEntrance
+end
+
 function ScoringMixin:AddKill()
+    if GetGameInfoEntity():GetWarmUpActive() then return end
 
     if not self.kills then
         self.kills = 0
-    end    
+    end
     if not self.killstreak then
         self.killstreak = 0
-    end    
+    end
 
     self.kills = Clamp(self.kills + 1, 0, kMaxKills)
     self.killstreak = Clamp(self.killstreak + 1, 0, kMaxKills)
 
-    // Skulks With Shotguns: reward kills.
+    -- Skulks With Shotguns: reward kills.
     self:rewardKill()
 
     if self.clientIndex and self.clientIndex > 0 then
@@ -196,13 +222,14 @@ function ScoringMixin:AddKill()
 end
 
 function ScoringMixin:AddAssistKill()
+    if GetGameInfoEntity():GetWarmUpActive() then return end
 
     if not self.assistkills then
         self.assistkills = 0
-    end    
+    end
 
     self.assistkills = Clamp(self.assistkills + 1, 0, kMaxKills)
-    
+
 end
 
 function ScoringMixin:GetKills()
@@ -222,40 +249,73 @@ function ScoringMixin:GetDeaths()
 end
 
 function ScoringMixin:AddDeaths()
+    if GetGameInfoEntity():GetWarmUpActive() then return end
 
     if not self.deaths then
         self.deaths = 0
     end
 
-     // reset killstreak
+     -- reset killstreak
     self.killstreak = 0
     self.deaths = Clamp(self.deaths + 1, 0, kMaxDeaths)
-    
+
 end
 
-function ScoringMixin:SetEntranceTime()
-	local teamNumber = self:GetTeamNumber()
-	
-	if teamNumber ~= self.teamAtEntrance then
-		self.entranceTime = Shared.GetTime()
-		self.teamAtEntrance = teamNumber
-	end
+function ScoringMixin:GetRelativeRoundTime()
+    return math.max( 0, Shared.GetTime() - GetGameInfoEntity():GetStartTime() ) --done to prevent float underflow
 end
 
-function ScoringMixin:GetEntranceTime()
-    return self.entranceTime
-end
-
-function ScoringMixin:SetExitTime()
-    self.exitTime = Shared.GetTime()
-end
-
-function ScoringMixin:GetExitTime()
-    if not self.exitTime or self.entranceTime > self.exitTime then 
-        return
+function ScoringMixin:SetEntranceTime( teamNumber )
+    if teamNumber ~= nil and ( teamNumber == kTeam1Index or teamNumber == kTeam2Index ) then
+        self.teamAtEntrance = teamNumber
+        table.insert( self.weightedEntranceTimes[teamNumber], self:GetRelativeRoundTime() )
     end
-    
-    return self.exitTime
+end
+
+function ScoringMixin:SetExitTime( teamNumber )
+    if teamNumber ~= nil and ( teamNumber == kTeam1Index or teamNumber == kTeam2Index ) then
+        table.insert( self.weightedExitTimes[teamNumber], self:GetRelativeRoundTime() )
+    end
+end
+
+function ScoringMixin:GetWeightedPlayTime( forTeamIdx, roundTimeWeighted )
+
+    if forTeamIdx ~= kTeam1Index and forTeamIdx ~= kTeam2Index then
+        return 0
+    end
+
+    local weightedTime = 0
+    local aT = math.pow( 2, 1 / 600 )
+
+    --Since time-spent on teams doesn't exceed allotted gameTime, additive is used
+    if self.weightedEntranceTimes[forTeamIdx] and #self.weightedEntranceTimes[forTeamIdx] > 0 then
+
+        local entrance, exit
+        local te, tx = 1, 1
+        repeat
+            -- get entrance
+            repeat
+                entrance, te = self.weightedEntranceTimes[forTeamIdx][te], te + 1
+            until not entrance or not exit or entrance > exit -- Ensure non-paired times are skipped.
+
+            -- get corresponding exit
+            if entrance then
+                repeat
+                    exit, tx = self.weightedExitTimes[forTeamIdx][tx], tx + 1
+                until not exit or exit > entrance -- Ensure non-paired times are skipped.
+
+                if exit then
+                    local weight = math.pow( aT, -entrance ) - math.pow( aT, -exit )
+                    weightedTime = weightedTime + weight
+                end
+            end
+
+        until not entrance or not exit
+
+    end
+
+    return weightedTime / roundTimeWeighted
+
 end
 
 function ScoringMixin:ResetScores()
@@ -264,37 +324,47 @@ function ScoringMixin:ResetScores()
     self.kills = 0
     self.killstreak = 0
     self.assistkills = 0
-    self.deaths = 0    
-    
+    self.deaths = 0
+
     self.commanderTime = 0
     self.playTime = 0
     self.marineTime = 0
     self.alienTime = 0
 
-    self.entranceTime = Shared.GetTime()
-    self.exitTime = nil
-    
+    self.weightedEntranceTimes = {}
+    self.weightedEntranceTimes[kTeam1Index] = {}
+    self.weightedEntranceTimes[kTeam2Index] = {}
+
+    local teamNum = self:GetTeamNumber()
+    if teamNum ~= nil and teamNum == kTeam1Index or teamNum == kTeam2Index then
+        table.insert( self.weightedEntranceTimes[teamNum], self:GetRelativeRoundTime() )
+    end
+
+    self.weightedExitTimes = {}
+    self.weightedExitTimes[kTeam1Index] = {}
+    self.weightedExitTimes[kTeam2Index] = {}
+
 end
 
-// Only award the pointsGivenOnScore once the amountNeededToScore are added into the score
-// determined by the passed in name.
-// An example, to give points based on health healed:
-// AddContinuousScore("Heal", amountHealed, 100, 1)
+-- Only award the pointsGivenOnScore once the amountNeededToScore are added into the score
+-- determined by the passed in name.
+-- An example, to give points based on health healed:
+-- AddContinuousScore("Heal", amountHealed, 100, 1)
 function ScoringMixin:AddContinuousScore(name, addAmount, amountNeededToScore, pointsGivenOnScore)
 
     if Server then
-    
+
         self.continuousScores[name] = self.continuousScores[name] or { amount = 0 }
         self.continuousScores[name].amount = self.continuousScores[name].amount + addAmount
         while self.continuousScores[name].amount >= amountNeededToScore do
-        
+
             self:AddScore(pointsGivenOnScore, 0)
             self.continuousScores[name].amount = self.continuousScores[name].amount - amountNeededToScore
-            
+
         end
-        
+
     end
-    
+
 end
 
 if Server then
@@ -302,30 +372,32 @@ if Server then
     function ScoringMixin:SetTotalKills(totalKills)
         self.totalKills = math.round(totalKills)
     end
-    
+
     function ScoringMixin:SetTotalAssists(totalAssists)
         self.totalAssists = math.round(totalAssists)
     end
-    
+
     function ScoringMixin:SetTotalDeaths(totalDeaths)
         self.totalDeaths = math.round(totalDeaths)
     end
-    
+
     function ScoringMixin:SetPlayerSkill(playerSkill)
         self.playerSkill = math.round(playerSkill)
     end
-    
+
     function ScoringMixin:SetTotalScore(totalScore)
         self.totalScore = math.round(totalScore)
     end
-    
+
     function ScoringMixin:SetTotalPlayTime(totalPlayTime)
         self.totalPlayTime = math.round(totalPlayTime)
     end
-    
+
     function ScoringMixin:SetPlayerLevel(playerLevel)
         self.playerLevel = math.round(playerLevel)
-    end 
+    end
 
+    function ScoringMixin:SetTotalXP(playerLevel)
+        self.totalXP = math.round(playerLevel)
+    end
 end
-
